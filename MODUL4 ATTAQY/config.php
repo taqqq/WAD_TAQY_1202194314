7121457492cfcd56a8590e9f4988f9dcb75caaab
taqqq <91315772+taqqq@users.noreply.github.com>
2021-11-29 13:13:13 +0700

    Delete MODUL4 ATTAQY directory

--- MODUL4 ATTAQY/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "root", "", "MODUL4", 3308);
-
-if (!$conn) {
-    echo "<script> alert('Failed to Connect into Database') </script>";
-
-    die(mysqli_connect_error());
-}
